46bf7b087f5d59b2df1df368ded5067f3f19083f
Miles Larkot <mileslarkot@proton.me>
2025-11-10 05:23:28 +0100

    Initial stuffff

---
+++ src/main/java/tn/esprit/angulartraining/AngularTrainingApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.angulartraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/angulartraining/AngularTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.angulartraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
